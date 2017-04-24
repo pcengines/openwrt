d4223c54d75bf8ea25af36de79646f01730baadf
Kamil Wcislo <kamil.wcislo@3mdeb.com>
2017-04-24 21:49:15 +0200

    target: add PCEngines APU2 board support
    Signed-off-by: Kamil Wcislo <kamil.wcislo@3mdeb.com>

---
+++ target/linux/x86/pcengines/target.mk
@@ -0,0 +1,9 @@
+ARCH:=x86_64
+ARCH_PACKAGES:=x86_64
+BOARDNAME:=PCEngines APU2
+FEATURES:=ext4 pci usb
+MAINTAINER:=Kamil Wcislo <kamil.wcislo@3mdeb.com>
+
+define Target/Description
+        Build images for APU2/3 boards by PCEngines
+endef
